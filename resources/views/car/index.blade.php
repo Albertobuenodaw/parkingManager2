a37ecd5233bde76765e2120fc580c2c9afc6ff90
Alberto <abuenocr21dw@ikzubirimanteo.com>
2022-12-06 19:56:44 +0100

    Added a select to create Car for users

--- resources/views/car/index.blade.php
+++ resources/views/car/index.blade.php
@@ -10,6 +10,13 @@
             <input class="lightRounded" type="text" name="brand" placeholder="Brand here"><br>
             <label for="model">Model:</label><br>
             <input class="lightRounded" type="text" name="model" placeholder="Model here"><br>
+            <label for='user'>Choose user if you want to add an existing one.</label>
+            <select name="user" class="col-md-5 form-select" >
+                <option selected disabled="true">--select User</option>
+                @foreach ($users as $user )
+                <option value="{{$user->id}}">{{$user->name}}</option>
+                @endforeach
+            </select>
             <input type="submit" class = 'greyBlueBtn' value="+ Add Car"> 
             @if (session('message'))
                 <div class='alert alert-success'>{{ session('message') }}</div>

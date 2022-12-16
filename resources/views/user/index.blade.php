d90047c725bc290582ca4a54bec6a3b41cbf5dd8
Alberto <abuenocr21dw@ikzubirimanteo.com>
2022-12-16 19:14:47 +0100

    Added old values to inputs in the user.index view

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,14 +5,14 @@
         <form action='/user' method='post'> 
         @csrf
             <label for="nombre">Name:</label><br>
-            <input class="lightRounded" type="text" name="name" placeholder="Name here" required><br>
+            <input class="lightRounded" type="text" name="name" placeholder="Name here" required value={{old('name')}}><br>
             <label for="apellido">Last Name:</label><br>
-            <input class="lightRounded" type="text" name="lastName" placeholder="Last Name here"><br>
+            <input class="lightRounded" type="text" name="lastName" placeholder="Last Name here" value={{old('lastName')}}><br>
             <label for="email">Email:</label><br>
-            <input class="lightRounded" type="email" name="email" placeholder="Email here"><br>
+            <input class="lightRounded" type="email" name="email" placeholder="Email here" value={{old('email')}}><br>
             <input type="submit" class = 'greyBlueBtn' value="+ Añadir Usuario"> 
             @if (session('message'))
-                <div class='alert alert-success'>{{ session('message') }}</div>
+                <div class='alert alert-success'>Usuario creado correctamente</div>
             @endif
         </form>
 </div>
